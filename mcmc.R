71019e9bfd23529dc9dcd923d0adc639f30a23d1
DataGary <beere-popcorn0j@icloud.com>
2023-01-21 21:04:37 +0100

    update

--- mcmc.R
+++ mcmc.R
@@ -2,23 +2,25 @@
 data <- read.csv("data.csv")
 
 # Fit a linear regression model
-model <- lm(Sales ~ Advertising + Discounts + Temperature + Competitor_Ads, data)
-
-# Print the model summary
-summary(model)
+model_lm <- lm(Sales ~ Advertising + Discounts + Temperature + Competitor_Ads, data)
 
 # First, we need to install and load the rstanarm package
 #install.packages("rstanarm")
 library(rstanarm)
 
 # Specify the model
-model <- stan_glm(Sales ~ Advertising + Discounts + Temperature + Competitor_Ads, data = data,
+model_mcmc <- stan_glm(Sales ~ Advertising + Discounts + Temperature + Competitor_Ads, data = data,
                   family = gaussian(),
-                  prior = normal(0, 2),
-                  prior_intercept = normal(0, 2),
+                  prior = normal(2, 1),
+                  prior_intercept = normal(500, 40),
                   chains = 4, iter = 2000, warmup = 1000, seed = 123)
 
-summary(model)
+# Print the model summary
+summary(model_lm)
+summary(model_mcmc)
 
 # Extract the coefficients and credible intervals
-coef(model)
+coef(model_mcmc)
+
+# get credible intervals
+posterior_interval(model_mcmc)
